c2fa6ed45e1560dcc033cb07842c61eef85a2796
godafflig <tombenat99@hotmail.fr>
2024-08-22 17:30:29 +0200

    push

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,82 +1,78 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-
-  <div class="container">
-    <img
-      class="photo"
-      src="@/assets/photomoi.png"
-      alt="logo du festival du film scolaire"
-    />
-
-    <p>
-      Bonjour, je me présente je m'apelle Tom Benat je suis UI designer.
-      Développeur depuis deux, je suis passionné par le monde du digital, après
-      Une formation et des expériences dans la restauration, j'ai décidé de me
-      Réorienter dans le monde du digital plus particulièrement dans le web
-      <br /><br /><br />
-      Mes atouts sont : <br />Mon esprit d'équipe, je joue un foot depuis que je
-      suis tout petit donc pour moi l'esprit d'équipe, c'est primordial <br />
-      Ma motivation/perserverance : Je persévère en toutes circonstances, sans
-      jamais abandonner. <br />
-      Mon organisation : l'organisation est essentielle pour moi.
-    </p>
-  </div>
-
-  <div class="container">
-    <p>
-      Bienvenue sur mon blog. C'est ici que je partage mon expérience de stage
-      au sein du festival du film scolaire.<br />
-      Cette expérience me permet de valider ma deuxième année dans mon cursus du
-      bachelor à Ynov Campus.<br />
-      Ce blog a pour but de documenter toutes les tâches et problèmes que j'ai
-      pu rencontrer au cours de ma période de stage, qui s'est déroulée en
-      demi-journée du 1er avril au 21 juin.
-    </p>
-  </div>
-
-  <div class="festival">
-    <img src="@/assets/logoFNFS.png" alt="logo du festival du film scolaire" />
-  </div>
-
-  <div class="container">
-    <p>
-      Le festival du film scolaire c'est le seul festival de france qui permet
-      au etudiant de faire reconniatre le travila au pres du grand public <br />
-      de grand jurys sont present pour remttre des prix, comme nelosn monfort
-      rebecca marder ou encore manon bresch
-    </p>
+  <div class="blockbody">
+    <div class="container">
+      <div class="col-left">
+        <div class="row row1"></div>
+        <div class="row row2"></div>
+        <div class="row row3">
+          <div class="row-col left"></div>
+          <div class="row-col right"></div>
+        </div>
+      </div>
+      <div class="col-right">
+        <div class="row top"></div>
+        <div class="row bottom"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
-html,
-body {
+* {
   padding: 0;
   margin: 0;
-  border: 0;
-}
-.photo {
-  width: 300px;
+  box-sizing: border-box;
 }
 
-p {
-  font-size: large;
+.blockbody {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .container {
+  width: 1000px;
+  height: 700px;
+  padding: 1em;
   display: flex;
-  text-align: justify;
-  padding: 2em;
-  border: 3px solid black;
-  margin: 2em 30em 5em 30em;
-  border-radius: 100px;
+  gap: 1em;
 }
-.festival img {
-  width: 200px;
+
+.col-left {
+  flex: 2;
+  border-radius: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+}
+.col-right {
+  border-radius: 30px;
+  flex: 1;
+  border: 1px red solid;
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+}
+.row {
+  flex: 1;
+  border: 1px red solid;
+  border-radius: 30px;
+}
+
+.row3 {
+  display: flex;
+  border-radius: 30px;
+  gap: 1em;
+}
+
+.row-col {
+  flex: 1;
+  border: 2px red solid;
+  border-radius: 30px;
 }
 </style>
 
 <script>
 </script>
+
