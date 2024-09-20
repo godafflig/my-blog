b6952cf1f8afdaa7aa80ccaf80cb5de73f8beaef
godafflig <tombenat99@hotmail.fr>
2024-09-20 13:39:07 +0200

    fiw router link test

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="blog">
-    <a href="/semaine1">1ère semaine - 01/04.05/04</a>
-    <a href="/semaine2">2ème semaine - 08/04.12/04</a>
-    <a href="/semaine3">3ème semaine - 15/04.19/04</a>
-    <a href="/semaine4">4ème semaine - 22/04.26/04</a>
-    <a href="/semaine5">5ème semaine - 29/04.03/05</a>
-    <a href="/semaine6">6ème semaine - 06/05.10/05</a>
-    <a href="/semaine7">7ème semaine - 13/05.17/05</a>
-    <a href="/semaine8">8ème semaine - 20/05.24/05</a>
-    <a href="/semaine9">9ème semaine - 27/05.31/05</a>
-    <a href="/semaine10">10ème semaine - 03/06.07/06</a>
-    <a href="/semaine11">11ème semaine - 10/06.14/06</a>
-    <a href="/semaine12">12ème semaine - 17/06.21/06</a>
+    <router-link href="/semaine1">1ère semaine - 01/04.05/04</router-link>
+    <router-link href="/semaine2">2ème semaine - 08/04.12/04</router-link>
+    <router-link href="/semaine3">3ème semaine - 15/04.19/04</router-link>
+    <router-link href="/semaine4">4ème semaine - 22/04.26/04</router-link>
+    <router-link href="/semaine5">5ème semaine - 29/04.03/05</router-link>
+    <router-link href="/semaine6">6ème semaine - 06/05.10/05</router-link>
+    <router-link href="/semaine7">7ème semaine - 13/05.17/05</router-link>
+    <router-link href="/semaine8">8ème semaine - 20/05.24/05</router-link>
+    <router-link href="/semaine9">9ème semaine - 27/05.31/05</router-link>
+    <router-link href="/semaine10">10ème semaine - 03/06.07/06</router-link>
+    <router-link href="/semaine11">11ème semaine - 10/06.14/06</router-link>
+    <router-link href="/semaine12">12ème semaine - 17/06.21/06</router-link>
   </div>
 </template>
 
